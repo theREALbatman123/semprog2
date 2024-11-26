0353c8b97d0ef9d0570bca43edd35f4ca47a459c
theREALbatman123 <oskar.jozsa@mensagymnazium.cz>
2024-11-26 17:32:34 +0100

    Create delegáti func.cs

---
+++ delegáti/delegáti func.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+
+		Func<double, double, double> násob = Multiply;
+		Console.WriteLine(Multiply(14, 5));
+	}
+
+	static double Multiply(double x, double y)
+	{
+		return x * y;
+	}
+}
