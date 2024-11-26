635b75a370756c590afc05a9d6cfd963419d90c7
theREALbatman123 <oskar.jozsa@mensagymnazium.cz>
2024-11-26 17:09:01 +0100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿delegate int Operace(int x , int y);
+class Program
+{
+	static void Main()
+	{
+		Operace operace = Scitej;
+		Operace operace2 = Odcitej;
+		Console.WriteLine(operace(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())));
+		Console.WriteLine(operace2(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())));
+
+	}
+	static int Scitej(int x, int y)
+	{
+		return x + y;
+	}
+	static int Odcitej(int x, int y)
+	{
+		return x - y;
+	}
+}
