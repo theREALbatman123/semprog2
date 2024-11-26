44f7903b394ab0312d87fa331a60e13679c49e9d
theREALbatman123 <oskar.jozsa@mensagymnazium.cz>
2024-11-26 17:15:59 +0100

    přidán delegát action

---
+++ delegáti/delegáti actions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+		Action<string> vytiskniZprávu = Tiskni;
+		vytiskniZprávu("Pepek námořník");
+	}
+
+	static void Tiskni(string message)
+	{
+		Console.WriteLine(message);
+	}
+}
