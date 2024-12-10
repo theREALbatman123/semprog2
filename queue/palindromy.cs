6a12bf5caf5fb772d0bc2df3908b6264a81a34c0
theREALbatman123 <oskar.jozsa@mensagymnazium.cz>
2024-12-10 17:26:52 +0100

    Create palindromy.cs

---
+++ queue/palindromy.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+class Program
+{
+	static void Main()
+	{
+		Console.WriteLine("Otestuj si svůj palindrom zde: ");
+		string vstup = Console.ReadLine().ToLower().Replace(" ", "");
+		Queue<char> palindromy = new Queue<char>();
+		foreach (char pismenko in vstup)
+		{
+			palindromy.Enqueue(pismenko);
+		}
+		bool totoPalindromJest = true;
+		int x = 0;
+		int y = palindromy.Count - 1;
+
+		while (x < y)
+		{
+			if (palindromy.Dequeue() != vstup[y])
+			{
+				totoPalindromJest = false;
+				break;
+			}
+			x++;
+			y--;
+		}
+		if (totoPalindromJest)
+		{
+			Console.WriteLine("Toto palindromem jest!!");
+		}
+		else
+		{
+			Console.WriteLine("Toto palindrom není");
+		}
+	}
+}
+
+
+		
