d79a9801048d9bcc8ac276079936f6c699914a45
theREALbatman123 <oskar.jozsa@mensagymnazium.cz>
2024-12-10 17:51:20 +0100

    vp np

---
+++ queue/vp np.cs
@@ -0,0 +1,34 @@
+﻿Queue<string> IMPORTANT = new Queue<string> ();
+Queue<string> NOTIMPORTANT = new Queue<string>();
+string vstup;
+while (true)
+{
+	vstup = Console.ReadLine ();
+	if (vstup == "STOP!!")
+	{
+		break;
+	}
+	if (vstup.StartsWith("vp"))
+	{
+		IMPORTANT.Enqueue(vstup);
+	}
+	else if (vstup.StartsWith("np"))
+	{
+		NOTIMPORTANT.Enqueue(vstup);
+	}
+	else
+	{
+		Console.WriteLine("imbecile");
+	}
+}
+int cislicko = 1;
+while (IMPORTANT.Count > 0)
+{
+	Console.WriteLine(cislicko + IMPORTANT.Dequeue());
+	cislicko ++;
+}
+while (NOTIMPORTANT.Count > 0)
+{
+	Console.WriteLine(cislicko + NOTIMPORTANT.Dequeue());
+	cislicko++;
+}
