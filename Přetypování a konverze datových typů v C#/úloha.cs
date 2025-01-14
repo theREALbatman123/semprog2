300ea84ad9501b28920bc9181e1946bc888153cb
theREALbatman123 <168565934+theREALbatman123@users.noreply.github.com>
2025-01-14 20:51:04 +0100

    Create úloha.cs

---
+++ Přetypování a konverze datových typů v C#/úloha.cs
@@ -0,0 +1,25 @@
+﻿double cislo = 5.75;
+Console.WriteLine($"číslo před přetypováním: {cislo}");
+
+int cislo2 = (int)cislo;
+Console.WriteLine($"číslo po explicitním přetypování: {cislo2}");
+
+
+float cislo3 = cislo2;
+Console.WriteLine($"číslo po implicitním přetypování {cislo3}");
+
+
+object pozdrav = "Dobrý den";
+string pozdrav2 = pozdrav as string;
+Console.WriteLine();
+Console.WriteLine(pozdrav2);
+
+
+if (pozdrav2 is string)
+{
+	Console.WriteLine("Ano. Toto vskutku je string.");
+}
+else
+{
+	Console.WriteLine("Ajajaj. Toto ovšem string není!");
+}
