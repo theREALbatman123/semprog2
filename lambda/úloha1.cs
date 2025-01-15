d4608caec656786dcf172e7f6c0c225dcfead3df
theREALbatman123 <168565934+theREALbatman123@users.noreply.github.com>
2025-01-15 18:58:09 +0100

    Create úloha1.cs

---
+++ lambda/úloha1.cs
@@ -0,0 +1,8 @@
+﻿List<int> cisla = new List<int> { 5, 15, 8, 22, 1, 11 };
+var velky = cisla
+	.Where(n => n > 10);
+Console.WriteLine("čísla větší než 10:");
+foreach (var n in velky)
+{ 
+	Console.WriteLine(n);
+}
