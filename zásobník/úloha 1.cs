9d2cc593c86e27b7e80e4bc0d9652fa95e98ea0e
theREALbatman123 <oskar.jozsa@mensagymnazium.cz>
2025-02-04 16:40:25 +0100

    Create úloha 1.cs

---
+++ zásobník/úloha 1.cs
@@ -0,0 +1,14 @@
+﻿Stack<char> zasobník = new Stack<char>();
+Console.WriteLine("napiš slovíčko: ");
+String slovo  = Console.ReadLine();
+char[] znaky = slovo.ToArray();
+
+foreach (char c in znaky)
+{
+	zasobník.Push(c);
+}
+Console.WriteLine();
+foreach (char c in znaky)
+{
+	Console.Write(zasobník.Pop());
+}
