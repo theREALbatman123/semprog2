e79f5989d6dd46de799191926fc0326f92adcb20
theREALbatman123 <oskar.jozsa@mensagymnazium.cz>
2025-02-04 17:45:22 +0100

    Create úloha 2.cs

---
+++ zásobník/úloha 2.cs
@@ -0,0 +1,64 @@
+﻿Stack<char> zásobník = new Stack<char>();
+Console.WriteLine("Napiš závorky:");
+string závorky = Console.ReadLine();
+char[] znaky = závorky.ToArray();
+
+foreach (char c in znaky)
+{
+	if (c == '(' || c == '{' || c == '[' || c == ')' || c == '}' || c == ']')
+	{
+		if (c == '(' || c == '{' || c == '[')
+		{
+			zásobník.Push(c);
+		}
+
+		if (c == ')')
+		{
+			if (zásobník.Peek() == '(')
+			{
+				zásobník.Pop();
+			}
+			else
+			{
+				Console.WriteLine("Chyba!");
+			}
+		}
+
+		else if (c == '}')
+		{
+			if (zásobník.Peek() == '{')
+			{
+				zásobník.Pop();
+			}
+			else
+			{
+				Console.WriteLine("Chyba!");
+			}
+		}
+
+		else if (c == ']')
+		{
+			if (zásobník.Peek() == '[')
+			{
+				zásobník.Pop();
+			}
+			else
+			{
+				Console.WriteLine("Chyba!");
+			}
+		}
+	}
+	else
+	{
+		Console.WriteLine("toto není závorka, mistře");
+	}
+	
+}
+if (zásobník == null)
+{
+	Console.WriteLine("vše v pořádku, pane");
+}
+else
+{
+	Console.WriteLine("vloudila se chybička, pane");
+}
