3521938237041309ac6f1ea2ddd27948ed6d3d56
theREALbatman123 <oskar.jozsa@mensagymnazium.cz>
2025-02-25 17:16:10 +0100

    Create úloha1.cs

---
+++ await/úloha1.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+
+class Program
+{
+	static async Task Main(string[] args)
+	{
+		Task synchronniOdpočet = AsyncFunction("odpocet", 10);
+		await Task.WhenAll(synchronniOdpočet);
+		Console.WriteLine("hotovo");
+	}
+	static async Task AsyncFunction(string name, int delay)
+	{
+		int x;
+		for (int i = delay; i > 0; i--)
+		{
+			await Task.Delay(1000);
+			x = i;
+			Console.WriteLine($"zbývá {x} vteřin");
+		}
+	}
+
+
+}
