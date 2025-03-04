1438d2f821470237f954ef47515f145eb4f7439d
theREALbatman123 <oskar.jozsa@mensagymnazium.cz>
2025-03-04 17:48:14 +0100

    postavička
    není hotové

---
+++ await/uloha2 (není hotová).cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using System.Net.Http;
+
+namespace ukol
+{
+	class program
+	{
+		static string ukol;
+		static async Task Main()
+		{
+			string[] url = { "https://pointerpointer.com/", "https://hackertyper.com/", "https://longdogechallenge.com/" };
+			Task<string> taska = StahniDataAsync(url[0], 0);
+			Task<string> taskb = StahniDataAsync(url[1], 1);
+			Task<string> taskc = StahniDataAsync(url[2], 2);
+			await Task.WhenAll(taska, taskb, taskc);
+			Console.WriteLine("hotovo"); 
+		}
+
+		static async Task<string> StahniDataAsync(string url, int h)
+		{
+			Console.WriteLine($"Ukol {h + 1} za chvíli bude");
+				using HttpClient client = new HttpClient();
+				string data = await client.GetStringAsync(url);
+			Console.WriteLine($"Ukol {h + 1} uz je");
+			return data;
+			}
+		}
+	}
---
+++ postavička/postavička.cs
@@ -0,0 +1,43 @@
+﻿int x = 7;
+int y = 6;
+int n;
+int m;
+n = x;
+m = y;
+
+while (true)
+{
+
+
+	
+	Console.Write("A");
+	Console.ReadKey();
+
+
+	if (Console.KeyAvailable)
+	{
+		Console.SetCursorPosition(x, y);
+		var key = Console.ReadKey(true).Key;
+
+		switch (key)
+		{
+
+			case ConsoleKey.UpArrow: if (y > 0) y--; break;
+			case ConsoleKey.DownArrow: if (y < Console.WindowHeight - 1) y++; break;
+			case ConsoleKey.LeftArrow: if (x > 0) x--; break;
+			case ConsoleKey.RightArrow: if (x < Console.WindowWidth - 1) x++; break;
+				
+		}
+
+	}
+
+	
+		
+		
+	Console.Write(" ");
+	Console.ReadKey();
+
+
+	
+	
+}
